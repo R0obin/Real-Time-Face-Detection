3025e92e31e5f7dba9ff34ed8c8cdabee3983ce6
Rabin Sunuwar <rabinj0oy@gmail.com>
2024-02-08 16:55:18 +0530

    Update

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import cv2 
-import flask as Flask
 
 # Load the face cascade classifier
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+eye_cascade = cv2.CascadeClassifier("haarcascade_eye_tree_eyeglasses.xml")
 
 # Open the video capture (0 for webcam)
 cap = cv2.VideoCapture(0)
@@ -12,23 +12,22 @@ while cap.isOpened():
     _, frame= cap.read()
 
     # Convert the frame to grayscale
-    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
      # Detect faces in the grayscale frame
-    faces = face_cascade.detectMultiScale(grey, 1.1, 2)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 2)
     # Draw rectangles around the detected faces
     for (x, y, w, h) in faces: 
-     cv2.rectangle(frame, (x,y), (x+w , y+h), (0, 255, 0),3)
-    # Check for 'q' key press to exit the loop
+        cv2.rectangle(frame, (x,y), (x+w , y+h), (0, 255, 0),3)
+        roi_gray= gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(255, 0, 0),3)
+        # Check for 'q' key press to exit the loop
     if cv2.waitKey(1) & 0xFF == ord('q'):
-       break
+        break
     cv2.imshow('frame',frame)
 
 # Release the video capture
 cap.release()
 cv2.destroyAllWindows()
-
-
-"""IF we want to detect recored video file or images we can use below methods"""
-
-
-
--- video.py
+++ video.py
@@ -1,17 +1,21 @@
 import cv2
 
 read_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+eyes_cascade = cv2.CascadeClassifier("haarcascade_eye_tree_eyeglasses.xml")
 video = cv2.VideoCapture('WhatsApp Video 2024-01-28 at 3.03.33 PM.mp4')
 
 while video.isOpened:        
     _, vid = video.read()
-
-    grey = cv2.cvtColor(vid,cv2.COLOR_BGR2GRAY)
-    frame = read_cascade.detectMultiScale(grey,1.1,3)
-
-
+    gray = cv2.cvtColor(vid,cv2.COLOR_BGR2GRAY)
+    frame = read_cascade.detectMultiScale(gray,1.1,3)
     for (x, y, w, h) in frame:
         cv2.rectangle(vid,(x, y),(x+w , y+h),(0, 255, 0),3)
+        roi_gray = gray[y:y+h,x:x+w]
+        roi_color = vid[y:y+h,x:x+w]
+        eyes = eyes_cascade.detectMultiScale(roi_color)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color,(ex, ey),(ex + ew , ey + eh),(255, 0, 0),3)
+
     cv2.imshow('vid',vid)
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
